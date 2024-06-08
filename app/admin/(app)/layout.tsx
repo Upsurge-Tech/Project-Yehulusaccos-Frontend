4aec9dd9c2c097953199df1ff7066f52efa27400
Yonatan <yon019hl@gmail.com>
2024-06-08 23:13:54 +0300

    fix: split to components

--- app/admin/(app)/layout.tsx
+++ app/admin/(app)/layout.tsx
@@ -1,25 +1,11 @@
-import Image from 'next/image'
-import Link from 'next/link'
 import { ReactNode } from 'react'
-import Logo2 from '@/public/assets/Logo2.png'
-import {
-  Sheet,
-  SheetClose,
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from '@/components/ui/sheet'
-import { HiMenuAlt2, HiX } from 'react-icons/hi'
-import { Button } from '@/components/ui/button'
 import { RiFileListFill } from 'react-icons/ri'
 import { MdAddBox } from 'react-icons/md'
-import { IoLogOut } from 'react-icons/io5'
-import { link } from 'fs'
-import NavLink from '@/components/NavLink'
+import { IconType } from 'react-icons'
+import AdminNavbar from '@/components/AdminNavBar'
+import AdminSideBar from '@/components/AdminSideBar'
 
-const navLinks = [
+const navLinks: { href: string; label: string; Icon: IconType }[] = [
   { href: '/admin/posts', label: 'All Posts', Icon: RiFileListFill },
   { href: '/admin/new-post', label: 'Add New', Icon: MdAddBox },
 ]
@@ -32,133 +18,13 @@ const Layout = ({ children }: Props) => {
   return (
     <div className='flex'>
       <div className='hidden md:block'>
-        <AdminSideBar />
+        <AdminSideBar navLinks={navLinks} />
       </div>
       <div className='flex-1'>
-        <AdminNavbar />
+        <AdminNavbar navLinks={navLinks} />
         <div>{children}</div>
       </div>
     </div>
   )
 }
 export default Layout
-
-const AdminSideBar = () => {
-  return (
-    <nav className='border h-screen py-9 flex flex-col items-center gap-9'>
-      <Link href='/home' className=''>
-        <Image
-          src={Logo2}
-          alt='imageLogo'
-          width={70}
-          height={70}
-          className='relative w-[50px] h-[50px] md:w-[70px]  md:h-[70px]'
-        />
-      </Link>
-
-      <div className='flex-1 flex flex-col'>
-        {navLinks.map(({ href, label, Icon }) => (
-          <NavLink
-            className=' flex justify-center '
-            activeClassName='bg-primary/10 text-primary'
-            nonActiveClassName=''
-            key={href}
-            href={href}
-          >
-            <Button
-              variant={'ghost'}
-              className='flex gap-2 items-center py-2 px-12'
-            >
-              <Icon className='text-2xl' />
-              <span>{label}</span>
-            </Button>
-          </NavLink>
-        ))}
-      </div>
-
-      <Button variant={'ghost'} className='w-full'>
-        <div className='flex gap-2 items-center'>
-          <IoLogOut className='text-2xl' />
-          <span className='text-md'>Logout</span>
-        </div>
-      </Button>
-    </nav>
-  )
-}
-
-const AdminNavbar = () => {
-  return (
-    <nav className='border'>
-      <div className='container flex items-center justify-between py-3 '>
-        <div className='hidden md:flex flex-col lg:flex-row lg:justify-end '>
-          <h1 className=''>
-            <span className='font-bold text-lg'>Welcome to admin panel, </span>
-            <span className='text-sm'>
-              Here you can view, add, edit, and delete news
-            </span>
-          </h1>
-        </div>
-        <Link href={'/news'} className='hidden md:inline-block'>
-          <Button className='shadow-lg shadow-primary/20'>
-            Visit On Website
-          </Button>
-        </Link>
-        <Link href='/home' className='md:hidden'>
-          <Image
-            src={Logo2}
-            alt='imageLogo'
-            width={70}
-            height={70}
-            className='relative w-[50px] h-[50px] md:w-[70px]  md:h-[70px]'
-          />
-        </Link>
-        <h1 className='md:hidden'>Admin Panel</h1>
-        <div className='md:hidden'>
-          <Sheet>
-            <SheetTrigger>
-              <HiMenuAlt2 className='text-3xl' />
-            </SheetTrigger>
-            <SheetContent className='flex flex-col'>
-              <SheetHeader>
-                <SheetTitle>Welcome to admin panel</SheetTitle>
-                <SheetDescription>
-                  Here you can view, add, edit, and delete news
-                </SheetDescription>
-              </SheetHeader>
-
-              <div className='flex-1 pt-6 flex flex-col gap-2'>
-                <Link href='/news'>
-                  <Button className='w-full'>Visit On Website</Button>
-                </Link>
-                {navLinks.map(({ href, label, Icon }) => (
-                  <NavLink
-                    className=' flex justify-center py-2'
-                    activeClassName='bg-primary/10  text-primary'
-                    nonActiveClassName=''
-                    key={href}
-                    href={href}
-                  >
-                    <SheetClose>
-                      <div className='flex gap-2 items-center'>
-                        <Icon className='text-2xl' />
-                        <span>{label}</span>
-                      </div>
-                    </SheetClose>
-                  </NavLink>
-                ))}
-                <div className='flex-1 flex flex-col justify-end'>
-                  <Button variant={'ghost'}>
-                    <div className='flex gap-2 items-center'>
-                      <IoLogOut className='text-2xl' />
-                      <span className='text-md'>Logout</span>
-                    </div>
-                  </Button>
-                </div>
-              </div>
-            </SheetContent>
-          </Sheet>
-        </div>
-      </div>
-    </nav>
-  )
-}
---
+++ components/AdminSideBar.tsx
@@ -0,0 +1,71 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import { ReactNode } from 'react'
+import Logo2 from '@/public/assets/Logo2.png'
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '@/components/ui/sheet'
+import { HiMenuAlt2, HiX } from 'react-icons/hi'
+import { link } from 'fs'
+import { Button } from '@/components/ui/button'
+import { RiFileListFill } from 'react-icons/ri'
+import { MdAddBox } from 'react-icons/md'
+import { IoLogOut } from 'react-icons/io5'
+import NavLink from '@/components/NavLink'
+import { IconType } from 'react-icons'
+import AdminNavbar from '@/components/AdminNavBar'
+
+const AdminSideBar = ({
+  navLinks,
+}: {
+  navLinks: { href: string; label: string; Icon: IconType }[]
+}) => {
+  return (
+    <nav className='border h-screen py-9 flex flex-col items-center gap-9'>
+      <Link href='/home' className=''>
+        <Image
+          src={Logo2}
+          alt='imageLogo'
+          width={70}
+          height={70}
+          className='relative w-[50px] h-[50px] md:w-[70px]  md:h-[70px]'
+        />
+      </Link>
+
+      <div className='flex-1 flex flex-col'>
+        {navLinks.map(({ href, label, Icon }) => (
+          <NavLink
+            className=' flex justify-center '
+            activeClassName='bg-primary/10 text-primary'
+            nonActiveClassName=''
+            key={href}
+            href={href}
+          >
+            <Button
+              variant={'ghost'}
+              className='flex gap-2 items-center py-2 px-12'
+            >
+              <Icon className='text-2xl' />
+              <span>{label}</span>
+            </Button>
+          </NavLink>
+        ))}
+      </div>
+
+      <Button variant={'ghost'} className='w-full'>
+        <div className='flex gap-2 items-center'>
+          <IoLogOut className='text-2xl' />
+          <span className='text-md'>Logout</span>
+        </div>
+      </Button>
+    </nav>
+  )
+}
+
+export default AdminSideBar
