8ffdfc6c83a56edb4eb0513b8c3a810e9266d02b
Yonatan <yon019hl@gmail.com>
2024-06-08 07:23:44 +0300

    feat: add shadcn theming

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from '@/components/ui/button'
+
 export default function Home() {
-  return <div>Welcome to Project Yehulu</div>;
+  return (
+    <div>
+      Welcome to Project Yehulu
+      <Button>hello</Button>
+    </div>
+  )
 }
