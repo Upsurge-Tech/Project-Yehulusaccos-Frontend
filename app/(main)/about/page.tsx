f9dc03f0c712167bfa92c7cb588948de86554aab
Mathias Wakgari <mathiaswakgari@gmail.com>
2024-06-10 11:50:31 +0300

    feat: Add About-Us page

--- app/(main)/about/page.tsx
+++ app/(main)/about/page.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 
+import AboutUs from "@/components/about/AboutUs";
+import Team from "@/components/about/Team";
+import OurOffice from "@/components/about/OurOffice";
+
 const AboutPage = () => {
-  return <div>AboutPage</div>;
+  return (
+    <div className="container my-5 md:my-10 lg:my-16 xl:my-36 flex flex-col items-center w-full overflow-hidden gap-y-5 md:gap-y-16 xl:gap-y-[250px] min-h-screen ">
+      <AboutUs />
+      <Team />
+      <OurOffice />
+    </div>
+  );
 };
 
 export default AboutPage;
