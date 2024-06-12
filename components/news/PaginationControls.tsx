8e4536c0a52cae50226d6cded1c983a16d5d4b94
Melakeselam Yitbarek <Melakeselamyitbarek2012@gmail.com>
2024-06-12 18:05:13 +0300

    edit the pagination

--- components/news/PaginationControls.tsx
+++ components/news/PaginationControls.tsx
@@ -1,37 +1,52 @@
 import { FaArrowLeftLong } from "react-icons/fa6";
 import { FaArrowRightLong } from "react-icons/fa6";
 
-const Pagination = ({ numPages, currentPage, onPageChange }: { numPages: number; currentPage: number; onPageChange: (page: number) => void }) => {
+const Pagination = ({ numPages, currentPage, onPageChange}: {numPages:number, currentPage:number, onPageChange: (page:number) => void}) => {
   const pages = Array.from({ length: numPages }, (_, i) => i + 1);
+  const leftPages = pages.slice(0, 2);
+  const rightPages = pages.slice(numPages - 2, numPages);
 
   return (
     <div className="w-full flex justify-between items-center text-primary">
       <button
         onClick={() => onPageChange(currentPage - 1)}
         disabled={currentPage === 1}
-        className={` ${currentPage === 1 ? "opacity-50": ""} flex justify-between items-center gap-x-3`}
+        className={` ${currentPage === 1 ? "opacity-50" : ""} flex justify-between items-center gap-x-3`}
       >
-        <FaArrowLeftLong size={25}/>
+        <FaArrowLeftLong size={25} className="hidden md:inline-block" />
         Oldest
       </button>
-      {pages.map(page => (
-        <button
-          key={page}
-          onClick={() => onPageChange(page)}
-          className={page === currentPage ? 'active' : ''}
-        >
-          {page}
-        </button>
-      ))}
+      <div className="flex gap-2">
+        {leftPages.map(page => (
+          <button
+            key={page}
+            onClick={() => onPageChange(page)}
+            className={page === currentPage ? 'active' : ''}
+          >
+            {page}
+          </button>
+        ))}
+        {numPages > 4 && <span>...</span>}
+        {rightPages.map(page => (
+          <button
+            key={page}
+            onClick={() => onPageChange(page)}
+            className={page === currentPage ? 'active' : ''}
+          >
+            {page}
+          </button>
+        ))}
+      </div>
       <button
         onClick={() => onPageChange(currentPage + 1)}
         disabled={currentPage === numPages}
-        className={` ${currentPage === numPages ? "opacity-50": ""} flex justify-between items-center gap-x-3`}
+        className={` ${currentPage === numPages ? "opacity-50" : ""} flex justify-between items-center gap-x-3`}
       >
         Newest
-        <FaArrowRightLong size={25} />
+        <FaArrowRightLong size={25} className="hidden md:inline" />
       </button>
     </div>
   );
 };
+
 export default Pagination;
