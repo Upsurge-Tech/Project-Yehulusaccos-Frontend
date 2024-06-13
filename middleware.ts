c6d872adc6a99332f391ad0a977388c4c85046b7
Yonatan <yon019hl@gmail.com>
2024-06-13 23:24:58 +0300

    fix: skip addding lang to admin routes and image urls

--- middleware.ts
+++ middleware.ts
@@ -1,22 +1,29 @@
-import { NextResponse } from 'next/server';
 import createMiddleware from "next-intl/middleware";
+import { NextRequest, NextResponse } from "next/server";
 
 const middleware = createMiddleware({
   locales: ["en", "am"],
   defaultLocale: "en",
 });
 
-export default async function (req) {
+export default async function (req: NextRequest) {
   const { pathname } = req.nextUrl;
 
   // Redirect from the root URL to /en/home or /am/home
-  if (pathname === '/') {
-    return NextResponse.redirect(new URL('/en/home', req.url));
+  if (pathname === "/") {
+    return NextResponse.redirect(new URL("/en/home", req.url));
+  }
+  if (pathname.startsWith("/admin")) {
+    return;
+  }
+  const images = ["jpg", "jpeg", "png", "gif", "svg", "webp", "ico"];
+  if (images.some((ext) => pathname.endsWith(ext))) {
+    return;
   }
 
   return middleware(req);
 }
 
 export const config = {
-  matcher: ['/((?!_next|favicon.ico|static|api).*)'],
+  matcher: ["/((?!_next|favicon.ico|static|api).*)"],
 };
