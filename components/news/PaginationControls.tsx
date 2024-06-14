b6d93d72d77e65374ac2eff7b3aa961c453e8011
Yonatan <yon019hl@gmail.com>
2024-06-14 22:44:17 +0300

    fix: remove ... if pages are not too many

--- components/news/PaginationControls.tsx
+++ components/news/PaginationControls.tsx
@@ -10,8 +10,12 @@ const Pagination = ({
   onPageChange: (page: number) => void;
 }) => {
   const pages = Array.from({ length: numPages }, (_, i) => i + 1);
-  const leftPages = pages.slice(0, 2);
-  const rightPages = pages.slice(numPages - 2, numPages);
+  console.log(pages);
+  const maxPages = 6;
+  const midPoint = Math.ceil(numPages / 2);
+  const span = Math.min(midPoint, Math.floor(maxPages / 2));
+  const leftPages = pages.slice(0, span);
+  const rightPages = pages.slice(numPages - span, numPages);
 
   return (
     <div className="w-full flex justify-between items-center text-primary">
@@ -33,7 +37,7 @@ const Pagination = ({
             {page}
           </button>
         ))}
-        {numPages > 4 && <span>...</span>}
+        {pages.length > maxPages && <span>...</span>}
         {rightPages.map((page) => (
           <button
             key={page}
