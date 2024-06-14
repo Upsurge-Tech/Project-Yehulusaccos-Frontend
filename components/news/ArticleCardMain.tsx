ae637f34504d784099928a8fc049b21662f34cb4
Yonatan <yon019hl@gmail.com>
2024-06-14 22:52:16 +0300

    feat: article cards are links

--- components/news/ArticleCardMain.tsx
+++ components/news/ArticleCardMain.tsx
@@ -1,6 +1,7 @@
 import { Article } from "@/data-types/Article";
 import formateDate from "@/utils/dateFormatter";
 import Image from "next/image";
+import Link from "next/link";
 import { IoIosArrowRoundUp } from "react-icons/io";
 
 const ArticleCardMain = ({ article }: { article: Article }) => {
@@ -22,10 +23,13 @@ const ArticleCardMain = ({ article }: { article: Article }) => {
         <span className="text-primary w-fit px-10 py-2 rounded-md text-center bg-[#00B6590D]">
           {formateDate(article.createdAt)}
         </span>
-        <div className="flex justify-between items-center gap-x-3">
+        <Link
+          href={`/news/${article.id}`}
+          className="flex justify-between items-center gap-x-3"
+        >
           <p className="font-bold">{article.title}</p>
           <IoIosArrowRoundUp className="text-primary rotate-45" size={40} />
-        </div>
+        </Link>
         <p>{article.excerpt}</p>
       </div>
     </div>
