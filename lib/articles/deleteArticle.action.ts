be522f97ca19267944ac2cee58b84f7bf9aee788
Yonatan <yon019hl@gmail.com>
2024-06-15 15:52:07 +0300

    fix: delete article deletes images and contents properly from db

--- lib/articles/deleteArticle.action.ts
+++ lib/articles/deleteArticle.action.ts
@@ -1,27 +1,25 @@
 "use server";
 
 import db from "@/db";
-import { ContentSQL, articleTable, contentTable } from "@/db/schema";
-import { and, eq } from "drizzle-orm";
+import { articleTable, contentTable } from "@/db/schema";
+import { eq } from "drizzle-orm";
+import getArticle from "./getArticle";
 import { removeFilesIfExist } from "./server-utils";
 
 const deleteArticle = async (id: number): Promise<{ error: string } | void> => {
-  let contents: ContentSQL[];
   try {
-    contents = await db
-      .select()
-      .from(contentTable)
-      .where(and(eq(contentTable.id, id), eq(contentTable.type, "image")));
-  } catch (e) {
-    let errString = "";
-    if (e instanceof Error) errString = e.message;
-    return { error: "Failed to delete article" + errString };
-  }
+    const result = await getArticle(id, false);
+    if ("error" in result) return result;
 
-  const imagePaths = contents.map((content) => content.data);
-  await removeFilesIfExist(imagePaths);
-  try {
-    await db.delete(contentTable).where(eq(contentTable.id, id));
+    const { article } = result;
+    const imagePaths: string[] = [
+      article.thumbnail,
+      ...article.contents
+        .map((c) => (c.type === "image" ? c.src : ""))
+        .filter((src) => src !== ""),
+    ];
+    await removeFilesIfExist(imagePaths);
+    await db.delete(contentTable).where(eq(contentTable.articleId, id));
     await db.delete(articleTable).where(eq(articleTable.id, id));
   } catch (e) {
     let errString = "";
