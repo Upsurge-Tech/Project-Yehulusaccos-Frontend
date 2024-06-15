b5a2bc89dd72db6ad18aacd6250671255e8c083e
Mathias Wakgari <104831041+mathiaswakgari@users.noreply.github.com>
2024-06-15 16:25:09 +0300

    fix: Resolve issue where animation would render before on screen

--- components/animation/PopUpAlt.tsx
+++ components/animation/PopUpAlt.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import React, { ReactNode } from "react";
-import { motion } from "framer-motion";
+import React, { ReactNode, useRef } from "react";
+import { motion, useInView } from "framer-motion";
 
 interface Props {
   children: ReactNode;
@@ -11,11 +11,15 @@ interface Props {
 }
 
 const PopUpAlt = ({ className, children, duration, delay }: Props) => {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
   return (
     <motion.div
+      ref={ref}
       initial={{ scale: 0 }}
-      animate={{ scale: [0, 1] }}
-      transition={{ duration: 3.5, ease: [0.42, 0, 0.58, 1] }}
+      animate={{ scale: isInView ? [0, 1] : 0 }}
+      transition={{ duration: 2.5, ease: [0.42, 0, 0.58, 1] }}
       className={className}
     >
       {children}
