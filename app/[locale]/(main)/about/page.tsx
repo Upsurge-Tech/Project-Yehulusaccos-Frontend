3b614c1635ba143a9ee7dbef4f833601fcdd2c46
Mathias Wakgari <104831041+mathiaswakgari@users.noreply.github.com>
2024-06-16 13:02:12 +0300

    style: Reduce Gaps

--- app/[locale]/(main)/about/page.tsx
+++ app/[locale]/(main)/about/page.tsx
@@ -6,7 +6,7 @@ import OurOffice from "@/components/about/OurOffice";
 
 const AboutPage = () => {
   return (
-    <div className="container my-5 md:my-10 lg:my-16 xl:my-36 flex flex-col items-center w-full overflow-hidden gap-y-5 md:gap-y-16 xl:gap-y-[250px] min-h-screen ">
+    <div className="container my-5 md:my-10 lg:my-16 xl:my-36 flex flex-col items-center w-full overflow-hidden md:gap-y-16 xl:gap-y-[250px] min-h-screen ">
       <AboutUs />
       <Team />
       <OurOffice />
