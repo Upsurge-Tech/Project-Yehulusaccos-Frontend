f840ca40bc385e3a8f2703a0157f6301a7e1a350
Yonatan <yon019hl@gmail.com>
2024-06-19 22:39:25 +0300

    fix: loan eligibility stepper and padding of load eligbility page

--- components/loan_eligibility/Stepper.tsx
+++ components/loan_eligibility/Stepper.tsx
@@ -1,75 +1,65 @@
-import React from "react";
+import { useTranslations } from "next-intl";
+import { IconType } from "react-icons";
 import { FaThumbsUp } from "react-icons/fa";
 import { FaMoneyBillTransfer } from "react-icons/fa6";
 import { ImLocation2 } from "react-icons/im";
 import { IoDocumentText } from "react-icons/io5";
-import { useTranslations } from "next-intl";
 import FadeIn from "../animation/FadeIn";
-import TitleFadeIn from "../animation/TitleFadeIn";
+
+const StepperRow = ({
+  Icon,
+  text,
+  number,
+}: {
+  Icon: IconType;
+  text: string;
+  number: number;
+}) => {
+  const isRight = number % 2 === 0;
+  return (
+    <li
+      className={`relative min-h-[150px] z-10 flex gap-2 items-center ${isRight ? "flex-row-reverse" : ""}`}
+    >
+      <div className="flex-1">
+        <FadeIn
+          delay={1.3}
+          className=" flex flex-col gap-2 justify-center items-center max-w-[500px] "
+        >
+          <Icon
+            size={35}
+            className="flex-1 text-primary font-medium leading-tight"
+          />
+          <p className="text-xs lg:text-sm text-center">{text}</p>
+        </FadeIn>
+      </div>
+
+      <span
+        className={`
+      flex items-center justify-center w-12 h-12  rounded-full
+      bg-white border-2 border-primary -start-6 ring-4 ring-white 
+        `}
+      >
+        {number}
+      </span>
+      <div className="flex-1 "></div>
+    </li>
+  );
+};
 
 const Stepper = () => {
   const tStepper = useTranslations("Loan.Stepper");
 
   return (
-    <ol className="w-full mt-10 relative left-36 lg:left-52 border-s-2 border-gray-300 ">
-      <li className="mb-10 ms-6 h-24">
-        <span className="absolute flex items-center justify-center w-12 h-12  rounded-full bg-white border-2 border-primary -start-6 ring-4 ring-white ">
-          1
-        </span>
-        <FadeIn className="" delay={1.3}>
-          <div className="relative flex flex-col gap-2 text-center right-[200px] lg:right-[360px] max-w-[175px] lg:max-w-xs ">
-            <IoDocumentText
-              size={35}
-              className="self-center text-primary font-medium leading-tight"
-            />
-            <p className="text-xs lg:text-sm self-end">{tStepper("Step1")}</p>
-          </div>
-        </FadeIn>
-      </li>
-      <li className="mb-10 ms-6 h-24">
-        <span className="absolute flex items-center justify-center w-12 h-12 rounded-full bg-white border-2 border-primary -start-6 ring-4 ring-white">
-          2
-        </span>
-        <FadeIn className="" delay={1.3}>
-          <div className="relative flex flex-col gap-2  items-center max-w-[15rem] text-center right-20 lg:right-auto">
-            <ImLocation2
-              size={35}
-              className="font-medium leading-tight text-primary"
-            />
-            <p className="text-xs lg:text-sm max-w-24 lg:max-w-xs">
-              {tStepper("Step2")}
-            </p>
-          </div>
-        </FadeIn>
-      </li>
-      <li className="mb-10 ms-6 h-24">
-        <span className="absolute flex items-center justify-center w-12 h-12 rounded-full bg-white border-2 border-primary -start-6 ring-4 ring-white ">
-          3
-        </span>
-        <FadeIn className="" delay={1.3}>
-          <div className="relative flex flex-col gap-2 text-center right-[183px] lg:right-[360px] max-w-36 lg:max-w-xs ">
-            <FaThumbsUp
-              size={35}
-              className="self-center text-primary font-medium leading-tight"
-            />
-            <p className="text-xs lg:text-sm self-end">{tStepper("Step3")}</p>
-          </div>
-        </FadeIn>
-      </li>
-      <li className="ms-6">
-        <span className="absolute flex items-center justify-center w-12 h-12 rounded-full bg-white border-2 border-primary -start-6 ring-4 ring-white ">
-          4
-        </span>
-        <FadeIn className="" delay={1.3}>
-          <div className="flex flex-col gap-2 items-center max-w-[4rem] text-center ">
-            <FaMoneyBillTransfer
-              size={35}
-              className="font-medium leading-tight text-primary"
-            />
-            <p className="text-xs lg:text-sm">{tStepper("Step4")}</p>
-          </div>
-        </FadeIn>
-      </li>
+    <ol className="w-full mt-10 relative ">
+      <div className="absolute w-[1px] h-[80%] top-1/2 left-1/2 bg-gray-300 translate-x-[-50%] translate-y-[-50%]"></div>
+      <StepperRow Icon={IoDocumentText} text={tStepper("Step1")} number={1} />
+      <StepperRow Icon={ImLocation2} text={tStepper("Step2")} number={2} />
+      <StepperRow Icon={FaThumbsUp} text={tStepper("Step3")} number={3} />
+      <StepperRow
+        Icon={FaMoneyBillTransfer}
+        text={tStepper("Step4")}
+        number={4}
+      />
     </ol>
   );
 };
